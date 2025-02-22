e346be28acdf2601d2201be15c57029336b281cd
Samay Pessoa <arolllysamay@gmail.com>
2025-02-22 18:29:05 -0300

    feat: model Cliente

---
+++ models/Cliente.py
@@ -0,0 +1,33 @@
+class Cliente(object):
+
+    @property
+    def id(self):
+        return self._id
+
+    @id.setter
+    def id(self, id):
+        self._id = id
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
+
+    @property
+    def cpf(self):
+        return self._cpf
+
+    @cpf.setter
+    def cpf(self, cpf):
+        self._cpf = cpf
+
+    @property
+    def endereco(self):
+        return self._endereco
+
+    @endereco.setter
+    def endereco(self, endereco):
+        self._endereco = endereco
